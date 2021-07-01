56448b989ae8db38d4dc719bbc83c38d343f4941
zhangzhuoru <46526069+zhangzhuoru@users.noreply.github.com>
2021-07-01 18:42:47 +0800

    add

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    // '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    "space-before-function-paren": 0,
+    "space-before-blocks": 0,
+    "key-spacing": 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <van-search
+      v-model="value"
+      shape="round"
+      background="#4fc08d"
+      placeholder="请输入搜索关键词"
+    />
+    <van-tabbar v-model="active" active-color="#ee0a24">
+      <van-tabbar-item name="home" icon="shop">首页</van-tabbar-item>
+      <van-tabbar-item name="search" icon="diamond">影视会员</van-tabbar-item>
+      <van-tabbar-item name="friends" icon="fire">生活缴费</van-tabbar-item>
+      <van-tabbar-item name="setting" icon="manager">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        active: 'home',
+        value:''
+      }
+    },
+    methods: {
+      onSearch(val) {
+        Toast(val);
+      },
+      onCancel() {
+        Toast('取消');
+      },
+    },
+  }
+</script>
